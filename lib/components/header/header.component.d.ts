36b2953970cac768fc7a1bbec7068e355008d13e
Matt Moore <me@mattdoescode.com>
2026-02-12 16:57:40 -0700

    debug log

--- lib/components/header/header.component.d.ts
+++ lib/components/header/header.component.d.ts
@@ -1,11 +1,12 @@
-import { ElementRef } from '@angular/core';
+import { AfterViewInit, ElementRef } from '@angular/core';
 import { DataTableComponent } from '../table/table.component';
 import * as i0 from "@angular/core";
-export declare class DataTableHeaderComponent {
+export declare class DataTableHeaderComponent implements AfterViewInit {
     dataTable: DataTableComponent;
     private elemRef;
     columnSelectorOpen: boolean;
     constructor(dataTable: DataTableComponent, elemRef: ElementRef);
+    ngAfterViewInit(): void;
     onClickHandler(event: any): void;
     onKeyUpHandler(event: any): void;
     onChange(event: Event): void;
